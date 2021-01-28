291da604fc1ed6c523df55cb57d738f71ef252a0
Minh KHOA <54676091+MinhKhoa2719@users.noreply.github.com>
2021-01-28 08:38:41 +0700

    set pull

--- MinhKhoa/demo-app/src/App.js
+++ MinhKhoa/demo-app/src/App.js
@@ -63,3 +63,19 @@ function About() {
 function Users() {
   return <h2>UsersUsersUsersUsers</h2>;
 }
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+<p>Note: We use href="#" for test links. In a real web site this would be URLs.</p>
+
+</body>
+</html>
